2d2662c562074e7cbf3d06a1d587a954027b5dd5
Kristopher Johnson <kris@kristopherjohnson.net>
2016-10-25 19:09:43 -0400

    Retrieve next week's schedule and filter by favorites

--- README.md
+++ README.md
@@ -6,9 +6,19 @@ Classic Movies, using TCM's Web Services APIs as described at
 <http://www.tcm.com/tcmws/v1/docs/welcome.html>.
 
 My goal is to have an automated service that notifies me when "something good"
-is showing on TCM so that I can set my DVR to record it.  It will look for
+is showing on TCM so that I can set my DVR to record it.  It looks for
 particular genres, actors, and directors.
 
 Requires [Node](http://nodejs.org).  Run `npm install` to install the
-dependencies, then execute `tcmws.js` to display what's currently showing on
-TCM.
+dependencies, then execute `tcmws.js` to display what's coming up in the next
+week on TCM.
+
+For an example of the output, see <https://gist.github.com/kristopherjohnson/2406522eb20a2639b85321a0a335e56e>.
+
+If your tastes vary from mine, just modify the definitions of `favoriteGenres`,
+`favoriteActors`, and `favoriteDirectors` in the script.
+
+I don't know why, but once in a while the TCM web service returns invalid
+responses with a 200 status code.  If you get an error indicating that
+`body.tcm.title` is null, just try running the script again.
+
--- tcmws.js
+++ tcmws.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
+// This script retrieves the next week of scheduled movie airings from Turner
+// Classic Movies (TCM) and displays those movies that match a set of favorite
+// genres, actors, or directors, or have four-star ratings from Leonard Maltin.
+
 "use strict";
 
 const async = require('async');
+const dateformat = require('dateformat');
 const request = require('request');
 
 // See <http://www.tcm.com/tcmws/v1/docs/welcome.html>
+// for web service interface details.
 const tcmwsBase = 'http://www.tcm.com/tcmws/v1';
+
+const numberOfDays = 7;
 
 // List of available genres: <http://www.tcm.com/tcmws/v1/schedule/genres.json>
 const favoriteGenres = [
@@ -24,37 +32,145 @@ const favoriteGenres = [
 ];
 
 const favoriteActors = [
+    'Alan Arkin',
+    'Alec Guinness',
+    'Anthony Hopkins',
+    'Anthony Perkins',
+    'Anthony Quinn',
+    'Ava Gardner',
+    'Barbara Stanwyck',
     'Basil Rathbone',
     'Bela Lugosi',
     'Bette Davis',
+    'Boris Karloff',
     'Burt Lancaster',
     'Cary Grant',
+    'Cesar Romero',
+    'Charles Laughton',
     'Christopher Lee',
+    'Claire Trevor',
+    'Claude Rains',
+    'Clint Eastwood',
     'David Niven',
+    'Dean Martin',
+    'Deborah Kerr',
+    'Edward G. Robinson',
+    'Fred MacMurray',
+    'Fredric March',
+    'Gary Cooper',
+    'Gene Hackman',
+    'Gene Tierney',
+    'Gene Wilder',
+    'George Sanders',
+    'Glenn Ford',
+    'Greer Garson',
     'Gregory Peck',
+    'Groucho Marx',
+    'Harry Belafonte',
+    'Hedy Lamarr',
     'Henry Fonda',
     'Humphrey Bogart',
     'Ingrid Bergman',
-    'Katherine Hepburn',
+    'Jack Lemmon',
+    'James Coburn',
+    'James Garner',
+    'James Stewart',
+    'Jean Arthur',
+    'Joan Fontaine',
+    'John Carradine',
+    'Kirk Douglas',
+    'Lana Turner',
+    'Lauren Bacall',
+    'Lawrence Tierney',
+    'Lee Van Cleef',
+    'Marlene Dietrich',
+    "Maureen O'Hara",
+    'Max Von Sydow',
     'Orson Welles',
+    'Paul Newman',
     'Peter Cushing',
     'Peter Lorre',
+    "Peter O'Toole",
+    'Peter Sellers',
+    'Richard Crenna',
+    'Richard Harris',
+    'Richard Pryor',
     'Rita Hayworth',
+    'Robert Montgomery',
+    'Ronald Colman',
+    'Robert Redford',
+    'Sammy Davis Jr.',
+    'Sean Connery',
+    'Sidnwy Poitier',
+    'Sophia Loren',
+    'Steve McQueen',
     'Sydney Greenstreet',
+    'Tony Curtis',
+    'Toshiro Mifune',
+    'Vera Miles',
+    'Vincent Price',
+    'William Holden',
+    'William Powell',
 ];
 
 const favoriteDirectors = [
+    'Akria Kurosawa',
     'Alfred Hitchcock',
+    'Billy Wilder',
     'David Lean',
+    'Francis Ford Coppola',
+    'James Whale',
+    'John Ford',
+    'John Frankenheimer',
+    'John Huston',
     'John Sturges',
+    'Mel Brooks',
+    'Oliver Stone',
     'Orson Welles',
+    'Otto Preminger',
+    'Robert Altman',
+    'Sam Peckinpah',
+    'Sergio Leone',
+    'Sidney Lumet',
+    'Stanley Kubrick',
+    'Terence Fisher',
+    'Woody Allen',
 ];
 
-function names(arr) {
-    return arr.map(x => x.name).join(', ');
+const isFavoriteGenre = {};
+favoriteGenres.forEach(x => isFavoriteGenre[x] = true);
+
+const isFavoriteActor = {};
+favoriteActors.forEach(x => isFavoriteActor[x] = true);
+
+const isFavoriteDirector = {};
+favoriteDirectors.forEach(x => isFavoriteDirector[x] = true);
+
+// Return true if the program matches any of our favorite genres, actors, or directors.
+function matchesFavorites(program, title, credits) {
+    return title.genres.some(genre => isFavoriteGenre[genre]) ||
+        names(actors(credits)).some(actor => isFavoriteActor[actor]) ||
+        names(directors(credits)).some(director => isFavoriteDirector[director]);
 }
 
-// Retrieve title. Callback takes arguments (error, title).
+// Return current date, e.g., '2016-10-25'
+function currentDateString() {
+    const now = new Date();
+    return dateformat(now, 'yyyy-mm-dd');
+}
+
+// Return 'name' property of array elements.
+function names(arr) {
+    return arr.map(x => x.name);
+}
+
+// Return names as a comma-separated string.
+function namesString(arr) {
+    return names(arr).join(', ');
+}
+
+// Retrieve title information from web service.
+// Callback takes arguments (error, title).
 function getTitle(titleId, callback) {
     const options = {
         url: `${tcmwsBase}/title/${titleId}.json`,
@@ -73,8 +189,8 @@ function getTitle(titleId, callback) {
     });
 }
 
-// Retrieve credits.  Callback takes arguments (error, credits),
-// where credits is an array.
+// Retrieve credits information from web service.
+// Callback takes arguments (error, credits).
 function getCredits(titleId, callback) {
     const options = {
         url: `${tcmwsBase}/title/${titleId}/credits.json`,
@@ -93,44 +209,7 @@ function getCredits(titleId, callback) {
     });
 }
 
-// Retrieve Maltin review.  Callback takes arguments (error, review).
-function getMaltinReview(titleId, callback) {
-    const options = {
-        url: `${tcmwsBase}/title/${titleId}/maltinReview.json`,
-        json: true
-    };
-    request.get(options, (error, response, body) => {
-        if (error) {
-            callback(error);
-        }
-        else if (response.statusCode == 200) {
-            callback(null, body.tcm.title.maltinReview);
-        }
-        else {
-            callback(new Error(`Unexpected status code ${response.statusCode}`));
-        }
-    });
-}
-
-function getWhatsOnNow(callback) {
-    const options = {
-        url: `${tcmwsBase}/schedule/whatsOnNow/us.json`,
-        json: true
-    };
-    request.get(options, (error, response, body) => {
-        if (error) {
-            callback(error);
-        }
-        else if (response.statusCode == 200) {
-            callback(null, body.tcm.schedule.whatsOnNow);
-        }
-        else {
-            callback(new Error(`Unexpected status code ${response.statusCode}`));
-        }
-    });
-}
-
-// Get all data for a title.  Callback takes arguments (error, title, credits, review).
+// Get additional data for a title.  Callback takes arguments (error, title, credits).
 function getTitleInfo(titleId, callback) {
     async.parallel(
         [
@@ -153,16 +232,6 @@ function getTitleInfo(titleId, callback) {
                         done(null, credits);
                     }
                 });
-            },
-            done => {
-                getMaltinReview(titleId, (error, review) => {
-                    if (error) {
-                        done(error);
-                    }
-                    else {
-                        done(null, review);
-                    }
-                });
             }
         ],
         (error, results) => {
@@ -170,22 +239,41 @@ function getTitleInfo(titleId, callback) {
                 callback(error);
             }
             else {
-                callback(null, results[0], results[1], results[2]);
+                callback(null, results[0], results[1]);
             }
         }
     );
 }
 
+// Get daily schedules.
+// Callback takes arguments (error, schedulesArray).
+function getSchedules(callback) {
+    const options = {
+        url: `${tcmwsBase}/schedule/us/est/${currentDateString()}.json?days=${numberOfDays}&includes=maltin`,
+        json: true
+    }
+    request.get(options, (error, response, body) => {
+        if (error) {
+            callback(error);
+        }
+        else if (response.statusCode == 200) {
+            callback(null, body.tcm.schedules);
+        }
+        else {
+            callback(new Error(`Unexpected status code ${response.statusCode}`));
+        }
+    });
+}
+
 function logTitle(title) {
-    console.log(`Title: ${title.name}`);
     console.log(`Description: ${title.description}`);
     console.log(`Genres: ${title.genres.join(', ')}`);
 }
 
 function logCredits(credits) {
-    console.log(`Directors: ${names(directors(credits))}`);
-    console.log(`Writers: ${names(writers(credits))}`);
-    console.log(`Actors: ${names(actors(credits))}`);
+    console.log(`Directed by: ${namesString(directors(credits)) || 'N/A'}`);
+    console.log(`Written by: ${namesString(writers(credits)) || 'N/A'}`);
+    console.log(`Cast: ${namesString(actors(credits)) || 'N/A'}`);
 }
 
 function actors(credits) {
@@ -200,26 +288,55 @@ function writers(credits) {
     return credits.filter(x => x.roleName == 'WRITER');
 }
 
-function logMaltinReview(review) {
-    if (review.rating) {
-        console.log(`Maltin Rating: ${review.rating}`);
+function isFourStarMaltinRating(maltin) {
+    return (maltin && maltin.rating == '****');
+}
+
+function logMaltinRating(maltin) {
+    if (maltin && maltin.rating) {
+        console.log(`Maltin Rating: ${maltin.rating}`);
     }
 }
 
-function showWhatsOnNow() {
-    getWhatsOnNow((error, whatsOnNow) => {
+function showProgramIfInteresting(program, callback) {
+    const titleId = program.titleId;
+    getTitleInfo(program.titleId, (error, title, credits, review) => {
         if (error) {
             console.log(error);
+            callback(error);
         }
         else {
-            getTitleInfo(whatsOnNow.titleId, (error, title, credits, review) => {
+            if (isFourStarMaltinRating(program.maltin) || matchesFavorites(program, title, credits)) {
+                console.log('');
+                console.log(`${program.time} - ${program.name} (${program.releaseYear})`);
                 logTitle(title);
                 logCredits(credits);
-                logMaltinReview(review);
-            });
+                logMaltinRating(program.maltin);
+            }
+            callback();
         }
     });
 }
 
-showWhatsOnNow()
+function showSchedule(schedule, callback) {
+    console.log('');
+    console.log('----------');
+    console.log(schedule.startDate);
+    console.log('----------');
+    async.eachSeries(schedule.programs, showProgramIfInteresting, callback);
+}
 
+function showSchedules() {
+    getSchedules((error, schedules) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            async.eachSeries(schedules, showSchedule);
+        }
+    });
+}
+
+showSchedules();
+
+// vim: set ts=8 sw=4 tw=0 et :
