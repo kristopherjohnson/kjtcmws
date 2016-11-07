1398c2044aa6b640ff4317b60cc6347c763ad7ba
Kristopher Johnson <kris@kristopherjohnson.net>
2016-11-07 01:00:25 +0000

    Add scripts/updatetcm.sh

--- scripts/updatetcm.sh
+++ scripts/updatetcm.sh
@@ -4,6 +4,8 @@
 
 cd /home/ubuntu/kjtcmws
 
+# The script often fails due to an invalid response from TCM's
+# web services, so we retry a few times on failure.
 for i in {1..5}; do
   echo "Attempt $i on `date`:"
   PATH="/usr/local/bin:$PATH" ./tcmws.js --html -o /tmp/tcm.html
