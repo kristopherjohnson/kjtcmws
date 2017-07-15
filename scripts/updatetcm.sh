3aa88d64320ae028eb8e2f09f897cbcebd667e3a
Kristopher Johnson <kris@kristopherjohnson.net>
2017-07-15 18:38:46 +0000

    Update output path for Ubuntu 16.04 Apache

--- scripts/updatetcm.sh
+++ scripts/updatetcm.sh
@@ -10,7 +10,7 @@ for i in {1..5}; do
   echo "Attempt $i on `date`:"
   PATH="/usr/local/bin:$PATH" ./tcmws.js --html -o /tmp/tcm.html
   if [ $? -eq 0 ]; then
-    /bin/mv /tmp/tcm.html /var/www/tcm.html
+    /bin/mv /tmp/tcm.html /var/www/html/tcm.html
     echo "Success"
     break
   fi
